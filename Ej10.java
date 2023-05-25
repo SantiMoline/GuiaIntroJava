0bdd9266dc5a5f5c2f31062932660e9ec18b4824
Santiago Moline <molinesanti@gmail.com>
2023-05-24 22:00:24 -0300

    Ejercicio 10 finalizado

--- Ej10.java
+++ Ej10.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 
 public class Ej10 {
     public static void main(String[] args) {
-        int limit;
+        int limit, num;
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Ingrese un número entero positivo como límite: ");
+        limit = scan.nextInt();
 
         do {
-
-        } 
-
-
+            System.out.println("Ingrese un número entero: ");
+            num = scan.nextInt();
+            limit -= num; //Voy restando del límite el número ingresado hasta llegar a 0.
+        } while (limit > 0); 
 
         scan.close();
     }
